136cec4034f44b325e5c61cf0455238c774f1b61
Brandon Shihabi <brandon.shihabi@gmail.com>
2024-11-18 22:58:20 -0800

    Adds error handling to setup bat

--- setup.bat
+++ setup.bat
@@ -1,6 +1,13 @@
-.\Bin\Sharpmake\Sharpmake.Application.exe /sources('athena.sharpmake.cs')
-mkdir Code\Core\Engine\Generated
-mkdir Code\Core\Engine\Generated\Shaders
-mkdir Assets\Built
+@echo off
+.\Bin\Sharpmake\Sharpmake.Application.exe /sources('athena.sharpmake.cs') || goto :error
+if not exist "Code\Core\Engine\Generated\" mkdir Code\Core\Engine\Generated || goto :error
+if not exist "Code\Core\Engine\Generated\Shaders\" mkdir Code\Core\Engine\Generated\Shaders || goto :error
+if not exist "Assets\Built\" mkdir Assets\Built || goto :error
 echo "Success! Press spacebar to close"
 pause
+goto :EOF
+
+:error
+echo Failed with error %errorlevel%
+exit /b %errorlevel%
+
